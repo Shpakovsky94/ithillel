764f269a2e3e05ad194f99e16c1161170332c06b
oleks <shpakovsky94@gmail.com>
2022-12-12 00:21:08 +0200

    init

---
+++ target/classes/scripts/init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS  test;
+
+CREATE TABLE IF NOT EXISTS test.PERSON (
+                                       PK_PERSON_ID int NOT NULL AUTO_INCREMENT,
+                                       FIRST_NAME varchar(40),
+                                       LAST_NAME varchar(40),
+                                       AGE int,
+                                       CITY varchar(20),
+                                       PRIMARY KEY (PK_PERSON_ID)
+                                       );
+
+INSERT INTO test.PERSON (FIRST_NAME, LAST_NAME, AGE, CITY)
+VALUES ('Petro', 'Petryk', 28, 'Lviv'),
+       ('Zahar', 'Petryk', 26, 'Lviv');
