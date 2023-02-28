ae263b0c6fb71caf0c88685bd6f084b90feee100
oleks <shpakovsky94@gmail.com>
2023-02-28 19:28:04 +0200

    2023 add lecture 12

---
+++ src/main/java/DefaultValueTest.java
@@ -0,0 +1,21 @@
+public class DefaultValueTest {
+
+    static boolean val1;
+    static double val2;
+    static float val3;
+    static int val4;
+    static long val5;
+    static String val6;
+    static char val7;
+
+    public static void main(String[] args) {
+
+        System.out.println(val1);
+        System.out.println(val2);
+        System.out.println(val3);
+        System.out.println(val4);
+        System.out.println(val5);
+        System.out.println(val6);
+        System.out.println(val7);
+    }
+}
